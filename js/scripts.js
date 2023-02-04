5fc1c4ef0b2cdb9419f648898fe8a78c7b6ee1d9
ksope <105855785+ksope@users.noreply.github.com>
2023-02-04 16:24:05 +0100

    Add new list of pokemon objects to an array list
    Replaced the former code with an array list of pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello world');
+const pokemonList = [];
+pokemonList.push({name: "Bulbasaur", height: 7, types: ['grass', 'poison']});
+pokemonList.push({name: "Ivysaur", height: 10, types: ['grass', 'poison']});
+pokemonList.push({name: "Venusaur", height: 20, types: ['grass', 'poison']});
+pokemonList.push({name: "Caterpie", height: 3, types: ['bug', 'electric']});
+pokemonList.push({name: "Wartortle", height: 10, types: ['dark', 'steel']});
 
-let favoriteFood = "Fried Rice";
-document.write(favoriteFood);
