3ea760de2c64c4d1e659775449683814b646ba71
ksope <105855785+ksope@users.noreply.github.com>
2023-03-15 10:52:42 +0100

    Update modalContainer selector
    Updated the selector for the modalContainer global variable to #modalBody

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 let pokemonRepository = (function(){
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-  let modalContainer = document.querySelector('#modal-container');
+  let modalContainer = document.querySelector('#modalBody');
 
   //add a new pokemon to the pokemonList
   function add(pokemon){
